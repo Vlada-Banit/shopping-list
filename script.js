61e1eb5f6350db0b42b507437d0aa0eda7677214
Vlada-Banit <bani0101@mail.ru>
2024-04-08 09:07:30 +0300

    Домашнее задание 4
    попыталась сделать, но что-то запуталась. Ввод текста работает только на одну букву и на экран выводит как [object NodeList]

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-/* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('#input');
+const listContainer = document.querySelector('.groceries');
 
-/* Пустые элементы не должны добавляться */
 
-/* Если кликнуть на элемент списка, он зачеркивается */
+sendInput.addEventListener('keydown', function(event) {
+    const listText = sendInput.value;
+    if (event.key == 'Enter') {
 
-/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
+    }
 
-/* Очищать input после добавления нового элемента в список */
+    const listItems = document.querySelectorAll('.items');
+    listItems.textContent = listText;
+    /* Новые элементы должны добавляться в список по нажатию на Enter */
+
+    if (listText != '')
+        listContainer.append(listItems);
+    /* Пустые элементы не должны добавляться */
+
+    for (let item of listItems) {
+        item.addEventListener('click', function() {
+            item.classList.toggle('done');
+        })
+    }
+    /* Если кликнуть на элемент списка, он зачеркивается */
+
+    /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
+
+    sendInput.value = ''; /* Очищать input после добавления нового элемента в список */
+})
